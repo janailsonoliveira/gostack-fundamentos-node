aac5a5c3dff540e7d50da7999f0f9fd185e72483
Janailson Oliveira <janailson.oliveira@gmail.com>
2020-04-19 23:16:32 -0300

    Entrega do desafio fundamentos node

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string,
+  value: number,
+  type: "income" | "outcome"
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,23 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
+  public execute({title, value, type}: Request): Transaction {
     // TODO
+    const isValidType = ['income', 'outcome'].includes(type);
+    if(!isValidType){
+      throw Error('Type is not valid');
+    }
+    const { total } = this.transactionsRepository.getBalance();
+
+    if(!!total){
+      if(value > total && type === 'outcome'){
+        throw Error('Value is not valid');
+      }
+    }
+    
+    const transaction = this.transactionsRepository.create(title, value, type);
+    
+    return transaction;
   }
 }
 
